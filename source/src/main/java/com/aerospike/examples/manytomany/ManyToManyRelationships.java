c4290d56c9217adaf609f084d2d5b7dfea221fd0
Tim F <120514393+tim-aero@users.noreply.github.com>
2025-07-14 22:44:13 +1000

    Updated many-to-many use case

--- source/src/main/java/com/aerospike/examples/manytomany/ManyToManyRelationships.java
+++ source/src/main/java/com/aerospike/examples/manytomany/ManyToManyRelationships.java
@@ -1,10 +1,15 @@
 package com.aerospike.examples.manytomany;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Optional;
 import java.util.concurrent.ThreadLocalRandom;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 import com.aerospike.client.BatchResults;
 import com.aerospike.client.Bin;
@@ -16,9 +21,9 @@ import com.aerospike.client.Value;
 import com.aerospike.client.cdt.ListOperation;
 import com.aerospike.client.cdt.ListOrder;
 import com.aerospike.client.cdt.ListPolicy;
+import com.aerospike.client.cdt.ListReturnType;
 import com.aerospike.client.cdt.ListWriteFlags;
 import com.aerospike.client.policy.BatchPolicy;
-import com.aerospike.client.policy.Policy;
 import com.aerospike.client.policy.WritePolicy;
 import com.aerospike.examples.UseCase;
 import com.aerospike.examples.Utils;
@@ -72,6 +77,7 @@ public class ManyToManyRelationships implements UseCase{
      * Add a new account to the database. This will insert the values into the database and 
      * update all the customers who are listed as owners of this account to include the ownershipe
      * @param client - The Aerospike client instance
+     * @param mapper - The AeroMapper instance for getting namespace and set information
      * @param account - The Account to save
      * @param ownerIds - The list owner ids for this account
      * @return true if all oeprations were successful, false otherwise.
@@ -111,23 +117,129 @@ public class ManyToManyRelationships implements UseCase{
         });
     }
 
+    /**
+     * Determine any customers that the passed customer is related to via an account. That is, two customers are
+     * related to each other if they have at least one account which they are both owners of.
+     * @param client - The Aerospike client instance
+     * @param mapper - The AeroMapper instance for getting namespace and set information
+     * @param customerId - The customer id to find all related customers of.
+     * @return - a Map of CustomerId -> Count, where count is the number of customers the accounts have in common.
+     */
     public Map<String, Integer> getRelatedCustomers(IAerospikeClient client, AeroMapper mapper, String customerId) {
         Key customerKey = new Key(mapper.getNamespace(Customer.class), mapper.getSet(Customer.class), customerId);
-        return Utils.doInTransaction(client, txn -> {
-            Policy readPolicy = new Policy();
-            readPolicy.txn = txn;
-            Record record = client.get(readPolicy, customerKey, "accounts");
-            if (record != null) {
-                List<String> accountIds = (List<String>) record.getList("accounts");
-                
-            }
-            return null; 
+        String accountNs = mapper.getNamespace(Account.class);
+        String accountSet = mapper.getSet(Account.class);
+        
+        Record record = client.get(null, customerKey, "accounts");
+        if (record != null) {
+            List<String> accountIds = (List<String>) record.getList("accounts");
+            Key[] accountKeys = accountIds.stream()
+                    .map(id -> new Key(accountNs, accountSet, id))
+                    .toArray(Key[]::new);
+            
+            Record[] records = client.get(null, accountKeys, "owners");
+            
+            // Now use Java streams to process into the map, extracting elements out of the 
+            // lists of each record
+            List<Record> recordList = Arrays.asList(records);
+            Map<String, Integer> counts = recordList.stream()
+                    .flatMap(rec -> {
+                        List<String> list = (List<String>) rec.getList("owners");
+                        return list == null ? Stream.empty() : list.stream();
+                    })
+                    .filter(id -> !customerId.equals(id))
+                    .collect(Collectors.groupingBy(
+                            Function.identity(),
+                            Collectors.reducing(0, e->1, Integer::sum)
+                    ));
+            
+            return counts;
+        }
+        return Map.of(); 
+    }
+    
+    /**
+     * Get a list of accounts related to the passed customer id.
+     * @param client - The Aerospike client instance
+     * @param mapper - The AeroMapper instance for getting namespace and set information
+     * @param customerId - The customer id to find all related accounts of.
+     * @return - a list of accounts related to this customer, or null if the customer does not exist.
+     */
+    public List<String> getRelatedAccountIds(IAerospikeClient client, AeroMapper mapper, String customerId) {
+        Key customerKey = new Key(mapper.getNamespace(Customer.class), mapper.getSet(Customer.class), customerId);
+        Record record = client.get(null, customerKey);
+        if (record != null) {
+            return (List<String>) record.getList("accounts");
+        }
+        return null;
+    }
+    
+    /**
+     * Get a list of accounts related to the passed customer id.
+     * @param client - The Aerospike client instance
+     * @param mapper - The AeroMapper instance for getting namespace and set information
+     * @param customerId - The customer id to find all related accounts of.
+     * throws - a list of accounts related to this customer, or null if the customer does not exist.
+     */
+    public void removeAssociation(IAerospikeClient client, AeroMapper mapper, String customerId, String accountId) {
+        Key customerKey = new Key(mapper.getNamespace(Customer.class), mapper.getSet(Customer.class), customerId);
+        Key accountKey = new Key(mapper.getNamespace(Account.class), mapper.getSet(Account.class), accountId);
+        
+        // Remove the customerId from the account object, remove the account id from the customer object
+        Utils.doInTransaction(client, txn -> {
+           WritePolicy writePolicy = client.copyWritePolicyDefault();
+           writePolicy.txn = txn;
+           
+           // Remove the accountid from the customer
+           Record rec = client.operate(writePolicy, customerKey, ListOperation.removeByValue("accounts", Value.get(accountId), ListReturnType.EXISTS));
+           if (rec.getBoolean("accounts")) {
+               // Remove the recordid from the account
+               rec = client.operate(writePolicy, accountKey, ListOperation.removeByValue("owners", Value.get(customerId), ListReturnType.EXISTS));
+               if (!rec.getBoolean("owners")) {
+                   
+                   throw new IllegalStateException(String.format(
+                           "Account record for key '%s', should contain customer id '%s' in it's owners list, but does not",
+                           accountId, customerId));
+                   
+               }
+           }
+           else {
+               throw new IllegalStateException(String.format(
+                       "Customer record for key '%s', should contain account id '%s' in it's account list, but does not",
+                       customerId, accountId));
+           }
         });
+    }
+    
+    /**
+     * Show the map of customer relationships on the terminal.
+     * @param relationships
+     */
+    public void displayRelatedCustomers(Map<String, Integer> relationships) {
+        System.out.println(" Customer | Count");
+        System.out.println("----------+------");
+        for (Entry<String, Integer> entry : relationships.entrySet()) {
+            System.out.printf("%9s | %,3d\n", entry.getKey(), entry.getValue());
+        }
     }
     @Override
     public void run(IAerospikeClient client, AeroMapper mapper) throws Exception {
-        // TODO Auto-generated method stub
+        // Find all the people related to "Cust-1"
+        Map<String, Integer> result = getRelatedCustomers(client, mapper, "Cust-1");
+        System.out.printf("\nFinding all the customer related to cusomter 'Cust-1' (%d):\n", result.size());
+        displayRelatedCustomers(result);
         
+        List<String> accounts = getRelatedAccountIds(client, mapper, "Cust-1");
+        if (accounts != null && !accounts.isEmpty()) {
+            // This should always be the case
+            System.out.printf("\nRemoving association between customer 'Cust-1' and account '%s'\n", accounts.get(0));
+            removeAssociation(client, mapper, "Cust-1", accounts.get(0));
+            
+            result = getRelatedCustomers(client, mapper, "Cust-1");
+            System.out.printf("\nRelationships after association was removed (%,d):\n", result.size());
+            displayRelatedCustomers(result);
+            
+        }
     }
 
 }
